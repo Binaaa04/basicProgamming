a62473a0332fe7da645c4ca86e60e04fa12a01d4
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 17:09:27 +0700

    JS06_Practicum 1_Modify number 4

--- JS06_Selection2/Selection2Exp121.java
+++ JS06_Selection2/Selection2Exp121.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
  * Selection2Exp121
  */
 public class Selection2Exp121 {
-public static void main(String[] args) {
+ public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         int year;
-        System.out.println("Enter a leap year: ");
+        System.out.println("Enter the year: ");
         year = input21.nextInt();
 
-        if (year % 4 == 0)
-            if (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)) {
-                System.out.println("leap year");
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.println("a leap year");
+                } else {
+                    System.out.println("not a leap year");
+                }
+            } else {
+                System.out.println("a leap year");
             }
-        {
+        } else {
             System.out.println("not a leap year");
-
         }
         input21.close();
     }
-
 }
