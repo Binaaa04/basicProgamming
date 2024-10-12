06284ffd9967e963445d16d6eb10a3b273da6914
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 17:23:11 +0700

    JS07_Practicum 2_Modify

--- JS07_Looping1/WhileOvertimePay21.java
+++ JS07_Looping1/WhileOvertimePay21.java
@@ -1,48 +1,36 @@
 import java.util.Scanner;
 /**
- * WhileOvertimePay21
+ * ModifywhileOvertimePay1
  */
-public class WhileOvertimePay21 {
+public class ModifywhileOvertimePay1 {
+   public static void main(String[] args) {
+      Scanner input21 = new Scanner(System.in);
+      int numEmployee, overtimeHours;
+      String position;
+      int overtimePay = 0;
+      double totalOvertimePay = 0.0;
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        Scanner input21 = new Scanner(System.in);
-        int numEmployee, overtimeHours;
-        String position;
-        int overtimePay = 0;
-        double totalOvertimePay = 0.0; 
+      System.out.print("Employee number :");
+      numEmployee = input21.nextInt();
 
-        System.out.print("Employee number :");
-        numEmployee = input21.nextInt();
-
-        int i=0;
-        while (i<numEmployee){
-           System.out.print("Position of employee "+(i+1)+" (director, manager,staff) = ");
-           position = input21.next();
-           System.out.print(" Employee "+(i+1)+" overtime hours = ");
-           overtimeHours = input21.nextInt();
-           i++;
-           if (position.equalsIgnoreCase("director")) {
+      int i = 0;
+      while (i < numEmployee) {
+         System.out.print("Position of employee " + (i + 1) + " (director, manager,staff, progammer) = ");
+         position = input21.next();
+         System.out.print(" Employee " + (i + 1) + " overtime hours = ");
+         overtimeHours = input21.nextInt();
+         i++;
+         if (position.equalsIgnoreCase("director")) {
             continue;
-           } else if (position.equalsIgnoreCase("manager")) {
-            overtimePay = overtimeHours*1000000;
-           } else if (position.equalsIgnoreCase("staff")) {
-            overtimePay = overtimeHours*75000;
-           
-            
-           } {
-            
-           } {
-            
-           }
-           }
-           totalOvertimePay += overtimePay;
-           System.out.println("Total of overtime Pay"+totalOvertimePay);
-           
-        }
-    }
-    
-
-
+         } else if (position.equalsIgnoreCase("manager")) {
+            overtimePay = overtimeHours * 100000;
+         } else if (position.equalsIgnoreCase("staff")) {
+            overtimePay = overtimeHours * 75000;
+         } else if (position.equalsIgnoreCase("progammer")) {
+            System.out.println("invalid position!");
+         }
+      }
+      totalOvertimePay += overtimePay;
+      System.out.println("Total of overtime Pay = " + totalOvertimePay);
+   }
+}
