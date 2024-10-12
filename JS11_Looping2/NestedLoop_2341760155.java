ba4513ba8a423e65e766a1a16f5b055a5d11e8ec
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 20:53:12 +0700

    JS11_Practicum 5_Modify number 2(Foreach Loop)

--- JS11_Looping2/NestedLoop_2341760155.java
+++ JS11_Looping2/NestedLoop_2341760155.java
@@ -16,7 +16,6 @@ public class NestedLoop_2341760155 {
             }
             System.out.println();
         }
-
         for (int i = 0; i < temps.length; i++) {
             System.out.println("City: " + i);
             for (double temperature : temps[i]) {
