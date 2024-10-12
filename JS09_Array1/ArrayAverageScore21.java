0efd779b9efda2602b838096fd3463aa31255df4
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 17:45:48 +0700

    JS09_Practicum 3_Modify number 1

--- JS09_Array1/ArrayAverageScore21.java
+++ JS09_Array1/ArrayAverageScore21.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-
 /**
  * ArrayAverageScore21
  */
 public class ArrayAverageScore21 {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         int[] score = new int[10];
         double total = 0;
@@ -15,14 +14,18 @@ public class ArrayAverageScore21 {
         for (int i = 0; i < score.length; i++) {
             System.out.print("Enter student score " + (i + 1) + " : ");
             score[i] = input21.nextInt();
+            if (score[i] > 70) {
+                System.out.println("student score " + (i + 1) + " passed! ");
+                totalpassed++;
+            }
         }
-
         for (int i = 0; i < score.length; i++) {
             total += score[i];
 
             average = total / score.length;
             System.out.println("The class of the scores is: " + average);
         }
+        System.out.println("number of students who passed : " + totalpassed);
         input21.close();
     }
 }
