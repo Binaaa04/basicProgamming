e7f2f5278f00c3aaf3795c678136571512bc3190
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 17:28:08 +0700

    JS07_Practicum 3_Modify number 5

--- JS07_Looping1/DoWhileLeaveEntitlement21.java
+++ JS07_Looping1/DoWhileLeaveEntitlement21.java
@@ -1,42 +1,35 @@
 import java.util.Scanner;
 /**
- * ModifyDoWhilwLeaveEntitlement21
+ * ModifyDoWhilELeaveEntitlement21Exp3
  */
-public class ModifyDoWhilwLeaveEntitlement21 {
+public class ModifyDoWhilELeaveEntitlement21Exp3 {
+
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         String confirmation;
-        int totalLeaveEntitlement = 15;
-        int remainingLeavesEntitlement = totalLeaveEntitlement;
-        int requestLeavesDays = 0;
-        int leaveEntitlement;
+        int leaveEntitlement, totalnumberofdays;
 
-        System.out.print("Number of Leave Entitlement : ");
+        System.out.print(" Number of Leave Entitlement : ");
         leaveEntitlement = input21.nextInt();
-
         do {
             System.out.print("do you want to take a Leave (y/t)?");
             confirmation = input21.next();
+            if (confirmation.equals("t")) {
+                break;
+            }
             if (confirmation.equalsIgnoreCase("yes")) {
-                System.out.println("total leaves entitlement : " + totalLeaveEntitlement);
+                System.out.print("total number of days : ");
+                totalnumberofdays = input21.nextInt();
 
-                if (totalLeaveEntitlement <= leaveEntitlement) {
-                    totalLeaveEntitlement -= leaveEntitlement;
-                    System.out.println(
-                            "The remaining leave Entitlement : " + totalLeaveEntitlement + " is not insufficient!");
-                    System.out.println("You have taken " + leaveEntitlement + " days off");
-                    System.out.println("Input the number of days of leave you want: ");
-                    requestLeavesDays = input21.nextInt();
+                if (totalnumberofdays <= leaveEntitlement) {
+                    leaveEntitlement -= totalnumberofdays;
+                    System.out.println("The remaining leave Entitlement : " + leaveEntitlement);
+                } else {
+                    System.out.println("The remaining leave Entitlement is not insufficient!");
                     break;
-
                 }
             }
-        } while (requestLeavesDays > remainingLeavesEntitlement);
-        System.out.println("remaining Leaves Entitlement : " + leaveEntitlement + " hari");
-        System.out.println("You have taken " + requestLeavesDays + " leave day ");
-        requestLeavesDays -= leaveEntitlement;
-        System.out.println("Your remaining leave entitlement is now: " + requestLeavesDays + " days");
-        
+        } while (leaveEntitlement > 0);
         input21.close();
     }
 }
