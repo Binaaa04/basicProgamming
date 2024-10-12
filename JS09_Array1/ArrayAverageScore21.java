95ba787de6a141459b266b6bc19a0051878dd9f1
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 17:47:45 +0700

    JS09_Practicum 3_Modify number 2

--- JS09_Array1/ArrayAverageScore21.java
+++ JS09_Array1/ArrayAverageScore21.java
@@ -4,28 +4,37 @@ import java.util.Scanner;
  */
 public class ArrayAverageScore21 {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
-        int[] score = new int[10];
-        double total = 0;
-        double average;
-        int totalpassed = 0;
+        int[] score = new int[5];
+        double totalpassed = 0;
+        double totalfailed = 0;
+        double averagepassed;
+        double averagefailed;
+        double totalgradepassed = 0;
+        double totalgradefailed = 0;
+        int totalstudents = 5;
+
+        System.out.println("Enter the number of students : " + totalstudents);
 
         for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter student score " + (i + 1) + " : ");
+            System.out.println("Enter the final score " + (i + 1) + " : ");
             score[i] = input21.nextInt();
-            if (score[i] > 70) {
-                System.out.println("student score " + (i + 1) + " passed! ");
+            total += score[i];
+
+            if (score[i] < 70) {
+                totalgradefailed += score[i];
+                totalfailed++;
+            } else if (score[i] > 70) {
+                totalgradepassed += score[i];
                 totalpassed++;
             }
         }
-        for (int i = 0; i < score.length; i++) {
-            total += score[i];
+        averagepassed = totalgradepassed / totalpassed;
+        System.out.println("The average score of students who passed is : " + averagepassed);
 
-            average = total / score.length;
-            System.out.println("The class of the scores is: " + average);
-        }
-        System.out.println("number of students who passed : " + totalpassed);
+        averagefailed = totalgradefailed / totalfailed;
+        System.out.println("The average score of students who failed is : " + averagefailed);
         input21.close();
     }
 }
