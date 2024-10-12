107542072b6d15601cc4455c1bec61dae29cbdc8
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 20:59:47 +0700

    JS11_Practicum 4_Modify number 3
    Modification so that it can display information about: input guess value entered by the user whether it is smaller or larger than the randomized answer/number.

--- JS11_Looping2/Quiz21.java
+++ JS11_Looping2/Quiz21.java
@@ -9,20 +9,33 @@ public class Quiz21 {
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         Random rand21 = new Random();
-        char menu = 'y';
+        char menu;
+
         do {
-            int number = rand21.nextInt (10) + 1; 
+            int number = rand21.nextInt(10) + 1;
             boolean success = false;
+
             do {
-                System.out.print("Guess the number (1-10): "); 
-                int answer = input21.nextInt(); 
-                input21.nextLine(); 
-                 success = (answer == number); 
+                System.out.print("Guess the number (1-10): ");
+                int answer = input21.nextInt();
+                input21.nextLine();
+                success = (answer == number);
+
+                if (!success) {
+                    System.out.println("Try again. Your guess is " + (answer < number ? "too low." : "too high."));
+                }
 
             } while (!success);
-                System.out.print("Do you want to repeat the game (Y/y)?"); 
-                menu = input21.next().charAt(0);
-                input21.nextLine();
-        } while (menu=='y' || menu=='Y');
+            System.out.print("Do you want to play again? (N/Y): ");
+            menu = input21.next().charAt(0);
+            input21.nextLine();
+
+            if (menu == 'N') {
+                System.out.println("Thanks for playing! Goodbye.");
+                break;
+            }
+
+        } while (menu == 'Y');
+        input21.close();
     }
 }
