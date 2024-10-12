3235673258611ce3f17b1595d36fd9aa0a54568b
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 17:42:42 +0700

    JS09_Practicum 2_Modify

--- JS09_Array1/ArrayValue21.java
+++ JS09_Array1/ArrayValue21.java
@@ -12,11 +12,13 @@ public class ArrayValue21 {
         for (int i = 0; i < finalScore.length; i++) {
             System.out.print("Enter the final score " + i + " : ");
             finalScore[i] = input21.nextInt();
-
         }
+        
         for (int i = 0; i < finalScore.length; i++) {
             if (finalScore[i] > 70) {
                 System.out.println("Final score " + i + " passed! ");
+            } else {
+                System.out.println("Final score " + i + " failed! ");
             }
         }
         input21.close();
