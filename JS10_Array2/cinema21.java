4daadd9abf2dd784c79475c97e32d6d1c18a109f
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 18:09:50 +0700

    JS10_Practicum 1_Modify number 12

--- JS10_Array2/cinema21.java
+++ JS10_Array2/cinema21.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
  * cinema21
  */
 public class cinema21 {
- public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         String[][] audience = new String[4][2];
         audience[0][0] = "Amin";
@@ -25,9 +25,8 @@ public class cinema21 {
         for (int i = 0; i < audience.length; i++) {
             System.out.println("Length of row " + (i + 1) + ":" + audience[i].length);
         }
-        System.out.println("Audiences in the row 3: ");
-        for (String i : audience[2]) {
-            System.out.println(i);
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Audience in the row : " + (i + 1) + String.join(", ", audience[i]));
         }
         input21.close();
     }
