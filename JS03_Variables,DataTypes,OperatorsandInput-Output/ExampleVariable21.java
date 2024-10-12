eda943226207fb3aac4bff4274a875a3429c4252
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 16:29:32 +0700

    JS03_ Practicum 1_Use of Variables

---
+++ JS03_Variables,DataTypes,OperatorsandInput-Output/ExampleVariable21.java
@@ -0,0 +1,19 @@
+/**
+ * ExampleVariable21
+ */
+public class ExampleVariable21 {
+
+    public static void main(String[] args) {
+        String oneOfMyHobbies = "Baking";
+        boolean isSmart = true;
+        char gender = 'F';
+        Byte _age = 19;
+        double $ipk = 4.00, height = 1.62;
+        
+        System.out.println(oneOfMyHobbies);
+        System.out.println("Are you smart ? " + isSmart);
+        System.out.println("Gender: " + gender);
+        System.out.println("My current age is : " + _age);
+        System.out.println(String.format("My IPK is and my height is meters", $ipk, height));
+        }
+}
