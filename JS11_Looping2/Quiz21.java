a9e68967e687335ab3e660d439cf7571a8442df4
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-12 21:01:06 +0700

    JS11_Practicum 4_Modify number 2

--- JS11_Looping2/Quiz21.java
+++ JS11_Looping2/Quiz21.java
@@ -6,11 +6,11 @@ import java.util.Random;
  */
 public class Quiz21 {
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
         Random rand21 = new Random();
         char menu;
-
+    
         do {
             int number = rand21.nextInt(10) + 1;
             boolean success = false;
@@ -20,12 +20,8 @@ public class Quiz21 {
                 int answer = input21.nextInt();
                 input21.nextLine();
                 success = (answer == number);
-
-                if (!success) {
-                    System.out.println("Try again. Your guess is " + (answer < number ? "too low." : "too high."));
-                }
-
             } while (!success);
+
             System.out.print("Do you want to play again? (N/Y): ");
             menu = input21.next().charAt(0);
             input21.nextLine();
@@ -36,6 +32,7 @@ public class Quiz21 {
             }
 
         } while (menu == 'Y');
-        input21.close();
+
+        input21.close(); 
     }
 }
